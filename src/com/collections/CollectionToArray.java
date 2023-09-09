635964cd18f8d19f464ba4f31de25f9d71f11b87
raju <rajub70639@gmail.com>
2023-09-10 00:30:26 +0530

    collection program change

--- src/com/collections/CollectionToArray.java
+++ src/com/collections/CollectionToArray.java
@@ -17,19 +17,14 @@ public class CollectionToArray {
 	       str.add("ambika");
 	       str.add("trinayana");
 	       
-<<<<<<< HEAD
+
 	     Object[] arr=str.toArray();
 	     
 	     for (Object string : arr) {
 			System.out.print(string);
 		
-=======
-	       Object[]  obj=str.toArray();
-	       
-	       for (Object ob : obj) {
-			System.out.print(ob+" ,");
 			
->>>>>>> eddbbc1597dee335b2e6041bff9eb4892268c43f
+
 		}
 	       
 	    	
@@ -39,7 +34,7 @@ public class CollectionToArray {
 	       str1.add("priya");
 	       str1.add("tony");
 	       
-<<<<<<< HEAD
+
 	     Object[] arr1=str1.toArray();
 	     for (Object stn : arr1) {
 				System.out.println(stn);
@@ -52,10 +47,8 @@ public class CollectionToArray {
      mp.put(2,"charan");
     mp.put(3,"ambika");
      mp.put(4,"trinayana");
-     Object[] ar=mp.values().toArray();
-           for(Object s:ar){
-       System.out.println(s);
-=======
+     
+
 	       Object[]  object=str1.toArray();
 	       System.out.println(" ");
 	       for (Object ob : object) {
@@ -63,21 +56,5 @@ public class CollectionToArray {
 			System.out.print(ob+" ,");
 		}
 	       
-	  
-	     Map<Integer,String> mp=new HashMap<Integer,String>();
-	     
-         mp.put(1,"raju");
-         mp.put(2,"charan");
-         mp.put(3,"ambika");
-        mp.put(4,"trinayana");
-        System.out.println(" ");
-     Object[] ar=mp.values().toArray();
-           for(Object s:ar){
-        	   
-       System.out.print(s+" ,");
->>>>>>> eddbbc1597dee335b2e6041bff9eb4892268c43f
-      }
-	       
-	}
-
+           }  
 }
